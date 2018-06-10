6f1ca0ee0fda1ddf955714802678042b72e184e2
Dave Walk <daviddwalk@gmail.com>
2018-06-10 12:11:42 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>85</p>
+        <p>Your Home Score is an 87 - congrats! The higher the score, the more efficient we measure your home to be against homes like yours.</p>
+        <p>(line chart here)</p>
+      </div>
+    );
+  }
+}
+
+export default App;
