64d88fa81e64368bd1a1c1093e704758df7d3631
Dave Walk <daviddwalk@gmail.com>
2018-06-11 20:42:32 -0400

    adding line chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
+import { Line as LineChart } from 'react-chartjs';
 
 const REACT_APP_API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
@@ -57,6 +58,7 @@ class HomeResults extends Component {
       <Message text={this.state.scoreMessage}/>
       <Statistic stat={this.state.avgDailyUsage} label='Avg Daily Usage'/>
       <Statistic stat={this.state.avgDailyTemp} label='Avg Daily Temp'/>
+      <DailyUsageChart homeId={this.props.match.params.homeId}/>
     </div>
     );
  }
@@ -89,10 +91,57 @@ class Statistic extends Component {
   }
 }
 
-class LineChart extends Component {
+class DailyUsageChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.newDefaultState();
+    this.options = {
+      scaleShowGridLines: false,
+      datasetFile: false,
+      scaleBeginAtZero: true,
+      responsive: true
+    };
+  }
+
+  newDefaultState() {
+    return {
+          labels: [],
+          datasets: [
+          {
+            label: "Daily Energy Usage",
+            fillColor: "rgba(220,220,220,0.2)",
+			      strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: []
+          }
+          ]
+        };
+  }
+
+ componentWillMount() {
+   const homeId = this.props.homeId;
+    fetch(`${REACT_APP_API_ENDPOINT}/api/1/subscriptions/homescore/${homeId}/energy/usage/daily`)
+      .then(res => res.json())
+      .then(result => {
+        let newState = this.newDefaultState();
+        result.daily_energy_usage.forEach(d => {
+          const key = Object.keys(d)[0];
+          const day = key.split('/')[1];
+          newState.labels.push(day);
+          newState.datasets[0].data.push(d[key]);
+        });
+        this.setState(newState);
+      });
+  }
+
   render() {
     return (
-      <p>Line chart will go here</p>
+      <div>
+        <LineChart data={this.state} options={this.options} width="900px"/>
+      </div>
     );
   }
 }
