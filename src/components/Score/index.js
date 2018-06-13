c2e9239783629e97329a84a350d47407e2062699
Dave Walk <daviddwalk@gmail.com>
2018-06-12 22:25:11 -0400

    add score gauge

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -1,11 +1,18 @@
 import { Typography, Paper } from '@material-ui/core';
 import React, { Component } from 'react';
+import Gauge from 'react-svg-gauge';
+
+function getColor(score) {
+  if (score < 40) return '#ff2424';
+  if (score < 80) return '#ffff24';
+  return '#009300';
+}
 
 export default class Score extends Component {
   render() {
     return (
         <Paper>
-        <Typography variant='display3'>{this.props.score}</Typography>
+          <Gauge value={this.props.score} backgroundColor="#fff" label="" color={getColor(this.props.score)}/>
         </Paper>
     );
   }
